f7a2904464c910e32720d77ae01ebc78524097f5
Jorge Rojas <jrojas@shokworks.io>
2023-04-06 16:55:57 -0400

    Few changes and updating README.md file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # telegram_bot
+
+Primero, para crear el bot debes interactuar con el @BotFather para crear tu bot y obtener el API de telegram.
+
+Una vez tengas el API, usando postman debes inicializar el webhook del bot: https://api.telegram.org/bot{TELEGRAM_API_KEY}/setWebhook
+
+Deberías obtener una respuesta como esta:
+
+{
+    "ok": true,
+    "result": true,
+    "description": "Webhook was set"
+}
+
+Una vez inicializado, se debe subir el código a un proveedor cloud, en mi caso use AWS: Lambda, API Gateway y DynamoDB
--- lambda_bot.py
+++ lambda_bot.py
@@ -3,6 +3,10 @@ import openai
 import telebot
 import boto3
 from boto3.dynamodb.conditions import Key
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # Initialize OpenAI API, Telegram Bot, and DynamoDB
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -10,41 +14,63 @@ bot = telebot.TeleBot(os.environ['TELEGRAM_BOT_TOKEN'])
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
 
-# Get the question count for the user
-def get_question_count(user_id):
+# Get the user data
+def get_user_data(user_id):
     response = table.get_item(Key={'user_id': user_id})
-    return response['Item']['question_count'] if 'Item' in response and 'question_count' in response['Item'] else 0
+    return response['Item'] if 'Item' in response else None
 
-# Update the question count in DynamoDB
-def update_question_count(user_id, question_count):
-    table.put_item(Item={'user_id': user_id, 'question_count': question_count})
+# Update the question count and paid status in DynamoDB
+def update_user_data(user_id, username, question_count, has_paid):
+    table.put_item(Item={'user_id': str(user_id), 'username': str(username), 'question_count': question_count, 'has_paid': has_paid})
 
-# Handle /start command
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Hello! I'm an expert marketing specialist. I'm sassy and a bit funny. Ask me any digital marketing question!")
+# Check if the user has paid
+def has_user_paid(user_id):
+    user_data = get_user_data(user_id)
+    return user_data['has_paid'] if user_data and 'has_paid' in user_data else False
 
 # Handle text messages
-@bot.message_handler(func=lambda message: True)
 def ask_question(message):
     user_id = message.chat.id
-    question_count = get_question_count(user_id)
-    update_question_count(user_id, question_count + 1)
+    username = message.chat.username
+    user_data = get_user_data(str(user_id))
+    
+    #logging.info(f"ask_question called for user {user_id}")
+
+    if not user_data:
+        question_count = 0
+        has_paid = False
+    else:
+        question_count = user_data['question_count']
+        has_paid = user_data['has_paid']
+
+    if question_count >= 5 and not has_paid:
+        bot.send_message(user_id, "Haz alcanzado 5 preguntras gratis. Para continuar disfrutando de marketero, contacta al administrador para hacer tu donación: @Carlosdaniel")
+        return
+
+    question_count += 1
+    update_user_data(str(user_id), username, question_count, has_paid)
 
     # Get a response from OpenAI API
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"Act as an expert marketing specialist with a savvy but funny tone: {message.text}",
-        max_tokens=150,
-        n=1,
-        temperature=0.8,
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo", #"gpt-4",
+        messages=[
+            {"role": "system", "content": "Actua como un experto especialista en marketing digital, con un tono inteligente pero divertido. Responde de forma muy precisa, sin dejar de lado tu tono."},
+            {"role": "user", "content": message.text},
+        ]
     )
 
     # Send the response to the user
-    bot.send_message(user_id, response.choices[0].text.strip())
+    bot.send_message(user_id, response['choices'][0]['message']['content'].strip())
+
 
 def lambda_handler(event, context):
-    bot.process_new_messages([telebot.types.Update.de_json(event["body"])])
+    #logger.info(f"Event: {event}")
+    update = telebot.types.Update.de_json(event["body"])
+    message = update.message or update.edited_message  # This line handles both regular messages and edited messages
+    #logger.info(f"Message: {message}")
+    
+    if message:
+        ask_question(message)
 
     return {
         "statusCode": 200,
